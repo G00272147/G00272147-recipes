f4ea35d08293839677360b5c4db763fe30fee1ca
G00272147 <G00272147@ATU.IE>
2025-12-28 22:49:23 +0000

    Add comments to favourite.page .html and .ts

--- src/app/pages/favourite/favourite.page.ts
+++ src/app/pages/favourite/favourite.page.ts
@@ -1,3 +1,4 @@
+//Load and display saved favourite recipes rom the local storage using StoreService
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular'
 import { CommonModule } from '@angular/common';
@@ -8,14 +9,17 @@ import { StoreService } from '../../services/store.service';
   selector: 'app-favourite',
   templateUrl: './favourite.page.html',
   standalone: true,
-  imports: [CommonModule, IonicModule, RouterModule],
+    imports: [CommonModule, IonicModule, RouterModule],
 })
 
 export class FavouritePage {
+  //Array to hold favourite recipes from local storage
   favs: any[] = [];
 
+  //Inject StoreService to read favourites from preferences
   constructor(private store: StoreService) { }
 
+  //Reloads favourites list so it updates after add/remove
   async ionViewWillEnter() {
     this.favs = await this.store.getFavourites()
   }
