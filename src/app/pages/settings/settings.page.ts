e8ca653779666f4037da7cec7cbe06e671485d96
G00272147 <G00272147@ATU.IE>
2025-12-28 22:58:26 +0000

    Add comments to settings.page .html and .ts

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,3 +1,5 @@
+// Allows user to change measurement settings (metric or US)
+// and saves the preference using StoreService so it persists after closing the app.
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
@@ -8,18 +10,24 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   selector: 'app-settings',
   templateUrl: './settings.page.html',
+
+  //routerModule is required so routerLine buttons work in HTML
   imports: [CommonModule, IonicModule, RouterModule],
 })
 
 export class SettingsPage {
+  //Stores selected measurement system (default is metric)
   measurement: 'metric' | 'us' = 'metric';
 
+  //Inject StoreService to load/save measurement preference
 constructor(private store: StoreService) { }
 
+//Loads previously saved measurement preference when entering the page (metric if none saved)
 async ionViewWillEnter() {
   this.measurement = await this.store.getMeasurement();
 }
 
+//Called when user changes measurement preference; saves new preference
 async changeMeasurement(value: 'metric' | "us") {
   this.measurement = value;
   await this.store.setMeasurement(value);
