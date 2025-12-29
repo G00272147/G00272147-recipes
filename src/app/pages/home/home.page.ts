2bf210a121f1ca7f8df479c3575a7034a6f35d45
G00272147 <G00272147@ATU.IE>
2025-12-29 01:58:04 +0000

    add 5 most recent searches to homepage with clear button

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { SpoonacularService } from '../../services/spoonacular';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   standalone: true,
@@ -19,27 +20,41 @@ export class HomePage {
   ingredients = ''; //enter text in ingredients input field
   results: any[] = []; //results returned from spoonacular API
   loading = false;
+  errorMsg = '';
 
-  constructor(private api: SpoonacularService, private router: Router) {}
+  //Recent searches (stored locally)
+  recentSearches: string[] = [];
 
-  errorMsg = ''; //error message to display if search fails
+constructor(
+    private api: SpoonacularService, 
+    private router: Router, 
+    private store: StoreService) {}
 
-  //Call SpoonacularService to search recipes by ingredients
-  search() {
-    const query = this.ingredients.trim();
-    if (!query) {
-      this.errorMsg = 'Please enter at least one ingredient.';
-      return;
-    }
+//Runs each time the home page opens (reload recent searches)
+async ionViewWillEnter() {
+  this.recentSearches = await this.store.getRecentSearches();
+}
+
+
+//Call SpoonacularService to search recipes by ingredients
+search() {
+  const query = this.ingredients.trim();
+  if (!query) {
+    this.errorMsg = 'Please enter at least one ingredient.';
+    return;
+   }
     
     this.errorMsg = '';
     this.loading = true;
     this.results = []; // Clear previous results
 
     this.api.searchRecipes(query).subscribe({
-      next: (res) => {
+      next: async (res) => {
       this.results = res;
       this.loading = false;
+
+      //Save this search to recent searches
+      await this.saveRecentSearch(query);
          },
       error: (err) => {
         console.error('SEARCH ERROR:', err);
@@ -47,6 +62,42 @@ export class HomePage {
         this.loading = false;
       },
     });
+}
+
+//Save recenet search to max 5, newest first, no duplicates
+private async saveRecentSearch(query: string) {
+  const q = query.trim();
+  if (!q) return;
+
+  //Normalise "a,b,c" to "a, b, c"
+const normalized = q
+.split(',')
+.map((x) => x.trim())
+.filter(Boolean)
+.join(', ');
+
+//Remove duplicates
+const withoutDuplicates = this.recentSearches.filter(
+  (s) => s.toLowerCase() !== normalized.toLowerCase()
+);
+
+//Add to start of list to keep only last 5 searches
+this.recentSearches = [normalized, ...withoutDuplicates].slice(0, 5);
+
+//Persist in Preferences
+await this.store.setRecentSearches(this.recentSearches);
+}
+
+//When user clicks a recent search, perform that search again
+useRecentSearch(s: string) {
+  this.ingredients = s;
+  this.search();
+}
+
+//clear recent searches list
+async clearRecentSearches() {
+  this.recentSearches = [];
+  await this.store.setRecentSearches([])
 }
 
 //Navigate to recipe details page
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -7,24 +7,44 @@ import { Preferences } from '@capacitor/preferences';
 export class StoreService {
   //Get saved measurement preference
   //If nothing saved yet, default to 'metric' 
+  private readonly MEASURMENT_KEY = 'measurement';
+  private readonly FAVOURITES_KEY = 'favourites';
+  private readonly RECENT_KEY = 'recentSearches';
+
+    //Get saved measurement preference
     async getMeasurement(): Promise<'metric' | 'us'> {
-        const  { value } = await Preferences.get({ key: 'measurement'});
+        const  { value } = await Preferences.get({ key: this.MEASURMENT_KEY});
           return (value as 'metric' | 'us') ?? 'metric';
   }
+
   //Save measurement preference
   async setMeasurement(m: 'metric' | 'us'): Promise<void> {
-    await Preferences.set({ key: 'measurement', value: m});
+    await Preferences.set({ key: this.MEASURMENT_KEY, value: m});
   }
 
   //Get saved favourite recipes. If nothing saved yet, return empty array
   async getFavourites(): Promise<any[]> {
-    const { value } = await Preferences.get({ key: 'favourites'});
+    const { value } = await Preferences.get({ key: this.FAVOURITES_KEY });
     return value ? JSON.parse(value) : [];
   }
   
   //Save favourite recipes list as JSON
   async setFavourites(favs: any[]): Promise<void> {
-    await Preferences.set({ key: 'favourites', value: JSON.stringify(favs) });
+    await Preferences.set({ 
+      key: this.FAVOURITES_KEY, 
+      value: JSON.stringify(favs) 
+    });
   }
 
-    }
+// Get the last 5 searches (returns [] if none saved yet)
+  async getRecentSearches(): Promise<string[]> {
+  const { value } = await Preferences.get({ key: this.RECENT_KEY });
+  return value ? JSON.parse(value) : [];
+}
+
+// Save the searches list
+async setRecentSearches(items: string[]): Promise<void> {
+  await Preferences.set ({ key: this.RECENT_KEY, value: JSON.stringify(items) });
+}    
+  }
+  
